4648f4b52fb96b59e412fb9ac01eff2cfeb33e7d
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-01-08 12:09:05 -0800

    remove deck class from cards_oop_functional.rb and add to blog

--- ruby/cards_oop_functional.rb
+++ ruby/cards_oop_functional.rb
@@ -11,28 +11,22 @@ class Card
   end
 end
 
-class Deck
-  SUITS = ['Heart', 'Diamond', 'Club', 'Spade']
-  VALUES = [
-    'Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-    'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'
-  ]
+def draw(deck, number_of_cards = 1, *already_drawn)
+  remaining_cards = deck - already_drawn.flatten
+  drawn_cards = remaining_cards.shuffle.sample(number_of_cards)
 
-  def initialize
-    @cards = SUITS.product(VALUES).map {|(suit, value)| Card.new(suit, value)}
-  end
-
-  def draw(number_of_cards = 1, *already_drawn)
-    remaining_cards = @cards - already_drawn.flatten
-    drawn_cards = remaining_cards.shuffle.sample(number_of_cards)
-
-    return drawn_cards
-  end
+  return drawn_cards
 end
 
-deck = Deck.new
-my_hand = deck.draw(5)
-your_hand = deck.draw(5, my_hand)
+suits = ['Heart', 'Diamond', 'Club', 'Spade']
+values = [
+  'Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+  'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'
+]
+
+deck = suits.product(values).map{|(suit, value)| Card.new(suit, value)}
+my_hand = draw(deck, 5)
+your_hand = draw(deck, 5, my_hand)
 
 puts "my hand is:"
 puts my_hand

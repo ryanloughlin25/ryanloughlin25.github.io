d39555557bc334eeaf06cf97d8c9765286c025ff
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-01-13 13:52:02 -0800

    week 7 technical blog post

---
+++ javascript/else_if.js
@@ -0,0 +1,32 @@
+//first
+var x = 15;
+
+if (x == 0)
+  console.log("zero");
+else if (x > 0)
+  console.log("positive");
+else
+  console.log("negative");
+
+//second
+var x = 15;
+
+if (x == 0)
+  console.log("zero");
+else {
+  if (x > 0)
+    console.log("positive");
+  else
+    console.log("negative");
+}
+
+//third
+var x = 15;
+
+if (x == 0)
+  console.log("zero");
+else
+  if (x > 0)
+    console.log("positive");
+  else
+    console.log("negative");

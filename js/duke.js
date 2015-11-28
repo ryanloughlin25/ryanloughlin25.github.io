e8cb724605e31903f7a47a8e0c2e2511a53510a9
Ryan Loughlin <rbloughlin@gmail.com>
2015-11-28 13:00:17 -0800

    start angular2, but should switch to typescript

--- js/duke.js
+++ js/duke.js
@@ -1,94 +1,12 @@
-angular.module('dukeApp', [])
-  .controller('GameController', function() {
-    var game = this;
-
-    game.board = [
-      [{}, {}, {}, {}, {}, {},],
-      [{}, {}, {}, {}, {}, {},],
-      [{}, {}, {}, {}, {}, {},],
-      [{}, {}, {}, {}, {}, {},],
-      [{}, {}, {}, {}, {}, {},],
-      [{}, {}, {}, {}, {}, {},],
-    ];
-
-    game.setAvailableMoves = function() {
-      angular.forEach(game.selectedPiece.square.getMoves(), function(move) {
-        console.log(move);
-        var r = +game.selectedPiece.row + move.y;
-        var c = +game.selectedPiece.column + move.x;
-        if (r >= 0 && r < 6 && c >= 0 && c < 6) {
-          console.log(r);
-          console.log(c);
-          var moveSquare = game.board[r][c]
-          if (!moveSquare.piece) {
-            moveSquare.moveType = 'move';
-          }
-        }
-      });
-    };
-
-    game.unsetAvailableMoves = function() {
-      console.log('unselect');
-      angular.forEach(game.board, function(row) {
-        angular.forEach(row, function(square) {
-          delete square.moveType;
-        });
-      });
-    }
-
-    game.movePiece = function() {
-      console.log('move');
-      game.board[game.selectedPiece.row][game.selectedPiece.column] = {};
-      game.board[game.clickedRow][game.clickedColumn] = game.selectedPiece.square;
-      if (game.selectedPiece.square.side === 'front') {
-        game.selectedPiece.square.side = 'back';
-      } else {
-        game.selectedPiece.square.side = 'front';
-      }
-    }
-
-    game.clickSquare = function(row, column, square) {
-      game.clickedRow = row;
-      game.clickedColumn = column;
-      if (square.piece) {
-        console.log(square);
-        game.selectedPiece = {
-          row: row,
-          column: column,
-          square: square,
-        };
-        game.setAvailableMoves();
-      } else if (square.moveType) {
-        if (square.moveType === 'move') {
-          game.unsetAvailableMoves();
-          game.movePiece();
-          game.selectedPiece = null;
-        }
-      }
-    };
-
-    //hardcode things
-    game.board[4][4] = {
-      piece: 'footman',
-      player: 'white',
-      side: 'front',
-      moves: {
-        front: [
-          {x: 0, y: -1},
-          {x: 0, y: 1},
-          {x: -1, y: 0},
-          {x: 1, y:0},
-        ],
-        back: [
-          {x: 0, y: -2},
-          {x: -1, y: -1},
-          {x: 1, y: -1},
-          {x: -1, y: 1},
-          {x: 1, y: 1},
-        ],
-      },
-      getMoves: function() {
-        return this.moves[this.side];
-      },
-    };
+var Duke = ng
+  .Component({
+    selector: 'duke-board',
+    template: '<h1>Hello</h1>',
+  })
+  .Class({
+    constructor: function () {},
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  ng.bootstrap(Duke);
+});

225c726673edc8cba1d653f7f698dff14e181d23
Ryan Loughlin <rbloughlin@gmail.com>
2015-12-24 17:33:31 -0800

    add a test for moving a footman twice (so that it flips from front to back to front)

--- spec/conf.js
+++ spec/conf.js
@@ -1,5 +1,6 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['duke_spec.js']
+  specs: ['duke_spec.js'],
+  rootElement: '.container'
 };
--- spec/duke_spec.js
+++ spec/duke_spec.js
@@ -1,20 +1,23 @@
-describe('move footman', function() {
-  var sourceSquare = element.all(by.className('square')).get(28);
-  var destinationSquare = element.all(by.className('square')).get(27)
-  var img = null;
+describe('Duke', function() {
+  function square(y, x) {
+    return element.all(by.className('square')).get(y*6 + x);
+  }
 
   beforeEach(function() {
-    browser.get('http://localhost:8000');
-    sourceSquare.click();
-    destinationSquare.click();
-    img = destinationSquare.element(by.tagName('img'));
+    browser.get('http://localhost:8080/src');
   });
 
-  it('should move a piece', function() {
-    expect(img.getAttribute('src')).toMatch(/footman/);
+  it('should load the page', function() {
+    expect(browser.getTitle()).toEqual('Duke');
   });
 
-  it('should flip a piece', function() {
-    expect(img.getAttribute('src')).toMatch(/back/);
+  it('should move a footman to the corner', function() {
+    square(2, 1).click();
+    square(1, 1).click();
+
+    square(1, 1).click();
+    var destination = square(0, 0);
+    destination.click();
+    expect(destination.getText()).toEqual('footman');
   });
 });
